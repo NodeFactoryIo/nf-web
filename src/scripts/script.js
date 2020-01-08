638b26e8d52e2c304ba8ee83c707723585627372
Belma Gutlic <belma.dzi@gmail.com>
2020-01-08 14:36:07 +0100

    Cookie policy banner

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -6,3 +6,20 @@ enableMenu();
 
 import enableCarousels from './carousels';
 enableCarousels();
+
+window.cookieconsent.initialise({
+    "palette": {
+        "popup": {
+            "background": "#252e39"
+        },
+        "button": {
+            "background": "#14a7d0"
+        }
+    },
+    "content": {
+        "message": "This website uses cookies because it's a necessary evil that helps us deliver our services and we have to let you know.",
+        "dismiss": "Got it!",
+        "link": "Learn more",
+        "href": "/cookiepolicy"
+    }
+});
