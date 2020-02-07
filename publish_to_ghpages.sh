2ee2526cfdf5a4fe526b8d738845e588e0edd07a
morrigan <belma.dzi@gmail.com>
2020-02-07 08:37:37 +0100

    Switch to public dir

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -7,22 +7,22 @@ then
 fi
 
 echo "Deleting old publication"
-rm -rf dist
-mkdir dist
+rm -rf public
+mkdir public
 git worktree prune
-rm -rf .git/worktrees/dist/
+rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages dist github/gh-pages
+git worktree add -B gh-pages public github/gh-pages
 
 echo "Removing existing files"
-rm -rf dist/*
+rm -rf public/*
 
 echo "Generating site"
-yarn build
+hugo
 
 echo "Updating gh-pages branch"
-cd dist && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
 #echo "Pushing to github"
 #git push --all
