ec6f486933e05172ed96383d324f542e0a5d918f
morrigan <belma.dzi@gmail.com>
2020-02-07 10:12:56 +0100

    Force push gh

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -25,4 +25,4 @@ echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
 #echo "Pushing to github"
-git push github gh-pages
+git push github gh-pages -f
